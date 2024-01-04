5bf850377e16b23f27a2ec4481bf61d76a0cb7a6
Zamzamke <zamzamchelimo@gmail.com>
2024-01-04 14:16:00 -0800

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * check_cycle - checks for cycles in a single-linked list
+ * @list: the single-linked list
+ * Return: 1(if present) else (0)
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *old;
+	listint_t *new;
+
+	if (list == NULL)
+	{
+		return (0);
+	}
+	old = list;
+	new = list;
+
+	while (new->next != NULL && new->next->next != NULL)
+	{
+		old = old->next;
+		new = new->next->next;
+
+		if (old == new)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
