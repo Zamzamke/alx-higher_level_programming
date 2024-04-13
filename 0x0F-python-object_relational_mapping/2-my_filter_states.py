028a22c70f521ba11cb80fe0b7e8f995a17ded44
Zamzamke <zamzamchelimo@gmail.com>
2024-04-13 13:22:31 -0700

    filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+takes in arguments and displays all values in the states table
+"""
+
+if __name__ == "__main__":
+    """
+    ake 4 arguments: mysql username, mysql password, database name
+    and state name searched
+    """
+    from sys import argv
+    import MySQLdb
+
+    dtb = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                          passwd=argv[2], db=argv[3])
+
+    cursor = dtb.cursor()
+
+    query = """SELECT * FROM states  WHERE name LIKE BINARY '{}'
+            ORDER BY states.id ASC""".format(argv[4])
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    dtb.close()
