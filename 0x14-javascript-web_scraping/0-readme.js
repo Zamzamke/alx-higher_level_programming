a0a46e514c6f10e86f255617f4fa51554b4a3f9c
Zamzamke <zamzamchelimo@gmail.com>
2024-06-26 15:15:52 -0700

    readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-<<<<<<< HEAD
 
 const fs = require('fs');
 
@@ -9,14 +8,4 @@ fs.readFile(process.argv[2], 'utf8', function (err, data) {
   } else {
     process.stdout.write(data);
   }
-=======
-const argv = process.argv;
-const fs = require('fs');
-fs.readFile(argv[2], 'utf-8', (error, data) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  console.log(data);
->>>>>>> f45b90d40957768ef5b9ecfc1edfb7a3af7d1eb2
 });
--- 0x14-javascript-web_scraping/README.md
+++ 0x14-javascript-web_scraping/README.md
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
+
 JavaScript Webscraping
-=======
-Javascript -WEB SCRAPING
->>>>>>> f45b90d40957768ef5b9ecfc1edfb7a3af7d1eb2
